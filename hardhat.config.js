f9ae7482d051800910791c2ef12d838f2c360b36
kumarchandan12 kumard <kumarchandan018u@gmail.com>
2024-08-01 02:55:01 +0000

    feat: initiated the project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [""],
+      accounts: ["0x133b44602862fadb8394a383b0aa4d159edbcaca8e15828743bc255dac7af486"],
     },
   },
 };
